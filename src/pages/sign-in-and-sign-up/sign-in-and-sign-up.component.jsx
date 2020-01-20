fb03e2adb29cbe54c09464d5c274e7de944d0653
brianramaswami <brianramaswami@yahoo.com>
2020-01-20 14:15:26 -0600

    working on signin

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './sign-in.styles.scss';
+import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
