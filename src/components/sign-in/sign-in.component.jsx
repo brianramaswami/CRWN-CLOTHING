92fd79918a1f2f4d13639f8d2e1fa29fece65486
brianramaswami <brianramaswami@yahoo.com>
2020-01-20 18:34:01 -0600

    form input

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './sign-in.styles.scss';
 
+import FormInput from '../form-input/form-input.component';
+
 class SignIn extends React.Component {
     constructor(props){
         super(props);
@@ -10,18 +12,50 @@ class SignIn extends React.Component {
             password: ''
         }
     }
+
+    handleSubmit= (event) =>{
+        event.preventDefault();
+
+        this.setState({
+            email: '',
+            password: ''
+        })
+    }
+
+    handleChange = (event) =>{
+        const {value, name} = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
     render(){
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form>
-                    <input name='email' value={this.state.email} required/>
-                    <label>Email</label>
-                    <input></input>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        handleChange = {this.handleChange} 
+                        value={this.state.email}
+                        label='email' 
+                        required/>
+                    <FormInput
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        handleChange = {this.handleChange}
+                        label='password'
+                        required
+                    />
+                    <input type='submit' value='Submit Form'/>
                 </form>
             </div>
         )
     }
 }
+
+export default SignIn;
